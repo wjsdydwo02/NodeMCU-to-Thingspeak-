2ec4278f46250f007ac801a3003abed7f4f80208
wjsdydwo02 <48196633+wjsdydwo02@users.noreply.github.com>
2019-03-31 23:25:47 +0900

    Hide personal info

--- WiFiClientTSandAWS.ino
+++ WiFiClientTSandAWS.ino
@@ -2,23 +2,7 @@
 #include <OneWire.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
-
-#ifndef STASSID
-#define STASSID "HOME"
-#define STAPSK  "12345678a!"
-#define WRITE_API_KEY "4GLHFIU5OI4KALEQ"
-#endif
-
-#define ONE_WIRE_BUS D3
-
-const char* ssid     = STASSID;
-const char* password = STAPSK;
-//Hosts to send data
-const char* host = "api.thingspeak.com";
-const uint16_t port = 80;
-const char* host2 = "ec2-52-79-239-180.ap-northeast-2.compute.amazonaws.com";
-const uint16_t port2 = 9000;
-
+#include "user_info.h"
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
