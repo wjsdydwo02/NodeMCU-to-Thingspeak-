cadba8e8524aafbd67fca00d7a8d661675847d48
wjsdydwo02 <48196633+wjsdydwo02@users.noreply.github.com>
2019-03-31 23:27:36 +0900

    hide personal info

--- user_info.h
+++ user_info.h
@@ -1,7 +1,7 @@
 #ifndef STASSID
 #define STASSID "HOME"
 #define STAPSK  "12345678a!"
-#define WRITE_API_KEY "4GLHFIU5OI4KALEQ"
+#define WRITE_API_KEY "vv12173_AP1_K3Y"
 #endif
 
 #define ONE_WIRE_BUS D3
@@ -11,5 +11,5 @@ const char* password = STAPSK;
 //Hosts to send data
 const char* host = "api.thingspeak.com";
 const uint16_t port = 80;
-const char* host2 = "ec2-52-79-239-180.ap-northeast-2.compute.amazonaws.com";
+const char* host2 = "ec2-your-ec2-address.compute.amazonaws.com";
 const uint16_t port2 = 9000;
